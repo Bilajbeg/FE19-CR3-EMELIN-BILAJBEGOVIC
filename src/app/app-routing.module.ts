e1a50e980cd35d216ff06b489bda1c25d8023546
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-06-23 18:32:40 +0200

    file dishes and Idishes created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { MenuComponent } from './menu/menu.component';
+import { DetailsComponent } from './details/details.component';
+import { CartComponent } from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "", component: HomeComponent
+},
+{
+  path: "about-us", component: AboutUsComponent
+},
+{
+  path: "menu", component: MenuComponent
+},
+{
+  path: "details", component: DetailsComponent
+},
+{
+  path: "cart", component: CartComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/dishes.ts
@@ -0,0 +1,33 @@
+export const dishes = [
+    {
+        img: '/assets/diavolo.jpg',
+        name: "Pizza Diavolo",
+        price: 12.50,
+        description: "In Italian, pizza diavola means “deviled” pizza. And in this case, deviled means one thing: spicy."
+    }, {
+        img: '/assets/margerita.jpg',
+        name: "Pizza Margerita",
+        price: 6.90,
+        description: "RAM 16G, CPU:snapdragon, CAMERA:40px"
+    }, {
+        img: '/assets/penne.jpg',
+        name: "Italian penne",
+        price: 5.90,
+        description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam"
+    }, {
+        img: '/assets/platter.jpg',
+        name: "Platter",
+        price: 15,
+        description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam"
+    }, {
+        img: '/assets/schrimps_spaghetti.jpg',
+        name: "Penne with scampi",
+        price: 16.80,
+        description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam"
+    }, {
+        img: '/assets/tiramisu.jpg',
+        name: "Spaghetti bolognese",
+        price: 11.90,
+        description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam"
+    },
+]
